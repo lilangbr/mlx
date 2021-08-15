a193b20aba463181fa7dc51c6158dc9ef2553d5c
lilangbr <eblang@gmail.com>
2021-08-15 17:05:01 -0300

    finished!

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: ebresser <ebresser@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 15:58:18 by ebresser          #+#    #+#             */
-/*   Updated: 2021/08/15 16:51:26 by ebresser         ###   ########.fr       */
+/*   Updated: 2021/08/15 17:04:44 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int new_frame(t_cub *cub)
 {   
     //texturas
-    //int		img_width;
-	//int		img_height;
-    //char	*relative_path = "./textures/doom_3.xpm";
+    int		img_width;
+	int		img_height;
+    char	*relative_path = "./textures/doom_3.XPM";
 
     
 
@@ -25,11 +25,11 @@ int new_frame(t_cub *cub)
 		mlx_destroy_image(cub->vars->mlx, cub->img->img );
 	
     
-    //texturas dá pau!
-    //cub->img->img = mlx_xpm_file_to_image(cub->vars->mlx, relative_path, &img_width, &img_height);
+    //texturas : olhar path direito!
+    cub->img->img = mlx_xpm_file_to_image(cub->vars->mlx, relative_path, &img_width, &img_height);
     
 
-    /*desenho  ok*/
+    /*desenho  ok
     cub->img->img = mlx_new_image(cub->vars->mlx, 600, 600);//init img
 	cub->img->addr = mlx_get_data_addr(cub->img->img, &(cub->img->bits_per_pixel),\
 		&(cub->img->line_length), &(cub->img->endian));
@@ -38,7 +38,7 @@ int new_frame(t_cub *cub)
 	draw_square2 (cub->img);
 	draw_opposite (cub->img);
 	draw_triangle (cub->img);
-    /**/
+    */
 
 
     
